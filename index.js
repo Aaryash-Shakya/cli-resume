37d6127706dfdd4d06f35be53f6e467d80d051ba
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-11-27 10:14:30 +0545

    add: basic intro using multiple console.log

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// function simplePrint() {
+//     return "Hello I am Aaryash Shakya";
+// }
 function delayPrint(message, delay) {
     return new Promise((resolve) => {
         setTimeout(() => {
-            console.log(message);
+            console.log(message)
             resolve();
         }, delay);
     });
@@ -9,11 +12,14 @@ function delayPrint(message, delay) {
 
 async function displayIntroduction() {
     await delayPrint("Hello! I'm Aaryash Shakya.", 1000);
-    await delayPrint("I'm a software developer with a passion for coding.", 1000);
-    await delayPrint("My favorite programming languages include JavaScript, Python, and Java.", 1000);
-    await delayPrint("I enjoy building web applications and exploring new technologies.", 1000);
-    await delayPrint("Feel free to connect with me on LinkedIn: https://www.linkedin.com/in/aaryash-shakya/", 1000);
+    await delayPrint("I'm a MERN Stack developer with a passion for coding.", 1000);
+    await delayPrint("I'm currently studying BSc. CSIT in Sagarmatha College of Science and Technology.", 1000);
+    await delayPrint("My favorite programming languages and frameworks are TypeScript, SCSS, and C++.", 1000);
+    await delayPrint("I enjoy building web applications and backend services.", 1000);
+    await delayPrint("Feel free to connect with me on the following platforms.", 1000);
+    await delayPrint("LinkedIn: https://www.linkedin.com/in/aaryash-shakya/", 1000);
+    await delayPrint("LinkedIn: https://www.github.com/Aaryash-Shakya/", 1000);
 }
 
 // Call the function to display the introduction
-displayIntroduction();
+module.exports = displayIntroduction;
