aacff0ea3d3078b6846fab57add8159a328acefb
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-12-04 14:53:09 +0545

    add: interface loop

--- index.js
+++ index.js
@@ -8,57 +8,6 @@ import nanospinner from "nanospinner";
 
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
-async function downloadAnimation(downloadName) {
-    const spinner = nanospinner.createSpinner(`Downloading ${downloadName}...`).start();
-    await sleep(2000);
-    spinner.success({ text: `${downloadName} Downloaded` });
-}
-
-async function welcome() {
-    setFiglet("_Aaryash_\n_Shakya_\n");
-    await sleep();
-    console.log(chalk.greenBright("\n\nHI! I am Aaryash Shakya."));
-    await sleep();
-    console.log("I am a MERN Stack developer.");
-    console.log("I am BSc. CSIT student at Sagarmatha College of Science and Technology.");
-    console.log("My favorite programming tools are TypeScript, SCSS, and C++.");
-    console.log("Feel free to connect with me on the following platforms.\n");
-    console.log("LinkedIn: https://www.linkedin.com/in/aaryash-shakya/");
-    console.log("Github: https://www.github.com/Aaryash-Shakya/");
-}
-
-async function question1() {
-    let ans = await inquirer.prompt({
-        name: "ans1",
-        type: "input",
-        message: "Would you like to know what MERN stands for? (y,n)",
-        default() {
-            return "y";
-        },
-    });
-    ans = ans.ans1.toLowerCase();
-    if (ans === "y" || ans === "yes") {
-        console.log("");
-        console.log(chalk.hex("#13aa52")("\tM  ->  MongoDB"));
-        sleep();
-        console.log(chalk.hex("#EFD81D")("\tE  ->  Express"));
-        sleep();
-        console.log(chalk.hex("#61DAFB")("\tR  ->  React"));
-        sleep();
-        console.log(chalk.hex("#6e9e42")("\tN  ->  Node.js"));
-        console.log("");
-        sleep(500);
-    } else {
-        console.log(chalk.bgRed("ok bye"));
-    }
-}
-
-// async function displayIntroduction() {
-
-// }
-
-// Call the function to display the introduction
-// module.exports = displayIntroduction;
 function setFiglet(message) {
     figlet(
         message,
@@ -81,13 +30,66 @@ function setFiglet(message) {
     );
 }
 
+async function downloadAnimation(downloadName) {
+    const spinner = nanospinner.createSpinner(`Downloading ${downloadName}...`).start();
+    await sleep(700);
+    spinner.success({ text: `${downloadName} Downloaded` });
+}
+
+async function welcome() {
+    setFiglet("_Aaryash_\n_Shakya_\n");
+    await sleep();
+    await downloadAnimation("User Profile");
+    await downloadAnimation("Contact Information");
+    await downloadAnimation("Skills and Expertise");
+    await downloadAnimation("Certifications");
+    await downloadAnimation("Project Details");
+    await downloadAnimation("Resume");
+    await downloadAnimation("Hobbies and Interests");
+    await sleep();
+}
+
+async function introduction() {
+    console.log(chalk.greenBright("\n\nHI! I am Aaryash Shakya."));
+    await sleep();
+    console.log("I am a MERN Stack developer.");
+    console.log("I am BSc. CSIT student at Sagarmatha College of Science and Technology.");
+    console.log("My favorite programming tools are TypeScript, SCSS, and C++.");
+    console.log("Feel free to connect with me on the following platforms.\n");
+    console.log("LinkedIn: https://www.linkedin.com/in/aaryash-shakya/");
+    console.log("Github: https://www.github.com/Aaryash-Shakya/");
+}
+
+async function userInterface() {
+    console.log(chalk.greenBright("\n+++++++++++++++++++++++++++++++++++++++++++++++++++"));
+    let exit = false;
+    let ans = await inquirer.prompt({
+        name: "option",
+        type: "list",
+        choices: ["Educational Background", "Contact Information", "Exit"],
+        message: "What would you like to know about me?",
+    });
+    switch (ans.option) {
+        case "Educational Background":
+            console.log("I am padante");
+            break;
+        case "Contact Information":
+            console.log("Social media");
+            break;
+        case "Exit":
+            exit = true;
+            break;
+        default:
+            "invalid option";
+    }
+    if (exit) {
+        console.log("Exiting");
+    } else {
+        userInterface();
+    }
+}
+
 // driver code using top level await
+console.clear();
 await welcome();
-await downloadAnimation("User Profile");
-await downloadAnimation("Contact Information");
-await downloadAnimation("Skills and Expertise");
-await downloadAnimation("Certifications");
-await downloadAnimation("Project Details");
-await downloadAnimation("Resume");
-await downloadAnimation("Hobbies and Interests");
-await question1();
+await userInterface();
